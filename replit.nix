a8b81d408a37fe928fd2e535e1ed8a92dadb06dc
arandomusernametouse <112630804+arandomusernametouse@users.noreply.github.com>
2022-09-02 18:30:27 +0530

    Initial Commit

---
+++ replit.nix
@@ -0,0 +1,11 @@
+{ pkgs }: {
+    deps = [
+        pkgs.gh
+        pkgs.yarn
+        pkgs.esbuild
+        pkgs.nodejs-16_x
+
+        pkgs.nodePackages.typescript
+        pkgs.nodePackages.typescript-language-server
+    ];
+}
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import * as core from '@actions/core';
+import * as fs from "fs";
+import fetch from "node-fetch";
+
+async function run(): Promise<void> {
+  try {
+    fetch("https://api.github.com/repos/akshanabha/trivia-app/releases/latest")
+    .then((data) => {
+      return data.json() as Object
+    })
+    .then((text) => {
+      core.info(JSON.stringify(text));
+      fs.writeFileSync("./data.json", JSON.stringify(text));
+    })
+  } catch (error) {
+    if (error instanceof Error)     core.setFailed(error.message)
+  }
+}
+
+run()
